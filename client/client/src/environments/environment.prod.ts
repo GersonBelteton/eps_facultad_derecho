7413eb77a68b6d859dcb7639337ce2de7316df7e
Gerson Belteton <gersonbelur@gmail.com>
2024-05-13 06:58:54 -0600

    instalacion

--- client/client/src/environments/environment.prod.ts
+++ client/client/src/environments/environment.prod.ts
@@ -8,8 +8,8 @@
 // };
 export const environment = {
   production: false,
-  basePath: 'http://ec2-18-118-247-210.us-east-2.compute.amazonaws.com/departamentales/user',
-  frontPath: 'http://ec2-18-118-247-210.us-east-2.compute.amazonaws.com/departamentales/back',
+  basePath: 'http://ec2-18-118-247-210.us-east-2.compute.amazonaws.com/departamentales/back/',
+  frontPath: 'http://ec2-18-118-247-210.us-east-2.compute.amazonaws.com/departamentales/user/',
   recaptcha: {
     siteKey: '6LdwVpclAAAAADp58Yoc-m2Y3k-WxuWgOfEF1bbO'
   },
